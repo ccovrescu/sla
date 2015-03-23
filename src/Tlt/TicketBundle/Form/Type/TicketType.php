fc253cdc0a9e4a228e7b3624167f939c01cd506b
Catalin <cbradescu@yahoo.com>
2015-03-23 14:36:23 +0200

    Fix nume sistem

--- src/Tlt/TicketBundle/Form/Type/TicketType.php
+++ src/Tlt/TicketBundle/Form/Type/TicketType.php
@@ -133,10 +133,10 @@ class TicketType extends AbstractType {
                 ));
             $builder->add(
                 'fixedAt', 'datetime', array(
-                    'date_widget' => "single_text",
                     'time_widget' => "single_text",
                     'label' => 'Data si ora rezolvarii:',
 //                    'data' => new \DateTime(),
+//                    'view_timezone' => 'Europe/Bucharest',
                 ));
             $builder->add(
                 'oldness', 'entity', array(
