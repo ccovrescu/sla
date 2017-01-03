bfd7c0615e8fec7b6c8e9ad3bc48f9648cf2a36f
Catalin Bradescu <cbradescu@yahoo.com>
2017-01-03 09:10:22 +0200

    Added 2017 to date inputs - reports - minor fix #2.

--- src/Tlt/MainBundle/Form/Type/SlaFiltersType.php
+++ src/Tlt/MainBundle/Form/Type/SlaFiltersType.php
@@ -57,8 +57,8 @@ class SlaFiltersType extends AbstractType
                     'format'=> 'dd.MM.yyyy',
                     'years' => array(
                         '2015',
-                        '2017',
-                        '2016'
+                        '2016',
+                        '2017'
                     ),
                     'label' => 'De la:'
                 )
